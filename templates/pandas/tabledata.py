2df649f4cac0d6ea09aa219a2fb641d3713efca8
safieoscar <65041538+safieoscar@users.noreply.github.com>
2020-10-27 15:44:44 -0600

    added final drafts

---
+++ templates/pandas/tabledata.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Read the csv file in
+df = pd.read_csv('WebVisualizations\Instructions\Resources\cities.csv')
+
+# Save to file
+df.to_html('data.html', index=False)
+
+# Assign to string
+table = df.to_html()
